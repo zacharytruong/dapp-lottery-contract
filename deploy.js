cc61a2747a645d8d9699ee9b139444ada95d2fef
Zachary Truong <zacharytruong@gmail.com>
2023-12-04 19:53:48 +0700

    complete the contract

---
+++ deploy.js
@@ -0,0 +1,27 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const { Web3 } = require('web3');
+const { abi, evm } = require('./compile');
+
+const provider = new HDWalletProvider(
+  'thrive diet bright example bench catch tank call stuff fossil fence crouch',
+  // remember to change this to your own phrase!
+  'https://sepolia.infura.io/v3/0c3b09f91cd941a3ace7f9292a84a025'
+  // remember to change this to your own endpoint!
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log(JSON.stringify(abi));
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
